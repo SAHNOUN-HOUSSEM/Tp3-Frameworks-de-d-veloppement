b38af05d0079c122d2834aa5e665aea1352c3eff
SahnounHoussem <sahnoun.houssem05@gmail.com>
2023-12-03 15:32:35 +0100

    tp3

---
+++ Data/ApplicationdbContext.cs
@@ -0,0 +1,27 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using tp3.Models;
+
+namespace tp3.Data
+{
+    public class ApplicationdbContext : DbContext
+    {
+        public ApplicationdbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Movies> movies { get; set; }
+        public DbSet<Genres> genres { get; set; }
+        public DbSet<Customers> customers { get; set; }
+        public DbSet<Membershiptypes> membershiptypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            string GenreJSon = System.IO.File.ReadAllText("Genres.Json");
+            List<Genres>? genres = System.Text.Json.JsonSerializer.Deserialize<List<Genres>>(GenreJSon);
+            foreach (Genres c in genres)
+                modelBuilder.Entity<Genres>()
+                .HasData(c);
+
+        }
+    }
+}
---
+++ Migrations/20231203140440_InitialCreate.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace tp3.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "membershiptypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SignUpFee = table.Column<double>(type: "float", nullable: false),
+                    DurationInMonth = table.Column<int>(type: "int", nullable: false),
+                    DiscountRate = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_membershiptypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    membershiptype_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_customers_membershiptypes_membershiptype_id",
+                        column: x => x.membershiptype_id,
+                        principalTable: "membershiptypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Movie_Added = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    genre_id = table.Column<int>(type: "int", nullable: true),
+                    file = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    CustomersId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_movies_customers_CustomersId",
+                        column: x => x.CustomersId,
+                        principalTable: "customers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_movies_genres_genre_id",
+                        column: x => x.genre_id,
+                        principalTable: "genres",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "genres",
+                columns: new[] { "Id", "GenreName" },
+                values: new object[,]
+                {
+                    { 1, "GenreFromJsonFile1" },
+                    { 2, "GenreFromJsonFile2" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customers_membershiptype_id",
+                table: "customers",
+                column: "membershiptype_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_movies_CustomersId",
+                table: "movies",
+                column: "CustomersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_movies_genre_id",
+                table: "movies",
+                column: "genre_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "movies");
+
+            migrationBuilder.DropTable(
+                name: "customers");
+
+            migrationBuilder.DropTable(
+                name: "genres");
+
+            migrationBuilder.DropTable(
+                name: "membershiptypes");
+        }
+    }
+}
